929018b392e757dfcc5b832b0d0601cc82d9749a
bitcores <bitcores@gmail.com>
2020-12-04 22:50:34 +0900

    Add files via upload

--- d4.py
+++ d4.py
@@ -7,106 +7,78 @@ inp = ""
 passports = []
 passports2 = []
 
-reqfields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-eyecolors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-
 ## Parse input
 with open("input4.txt") as fp:
     for line in fp:
         line = line.strip()
         
         if line == "":
-            passports.append(inp)
+            passports.append(inp.strip())
             inp = ""
         else:
             inp += line + " "
     if line != "":
-        passports.append(inp)
+        passports.append(inp.strip())
 
 ## Solve problem
-#print(passports)
-invalids = 0
-for x in passports:
-    valid = True
-    for y in reqfields:
-        if not y in x:
-            invalids += 1
-            valid = False
-            break
-    if valid:
-        passports2.append(x)
+def checkfields(psp):
+    for f in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]:
+        if not f in psp:
+            return False
+    return True
+
+def checkvalues(field, value):
+    if field in ["byr", "iyr", "eyr", "pid"]:
+        try:
+            val = int(value)
+        except:            
+            return False
+
+    if field == "byr":
+        if not 1920 <= val <= 2002: return False
+    if field == "iyr":
+        if not 2010 <= val <= 2020: return False
+    if field == "eyr":
+        if not 2020 <= val <= 2030: return False
+    if field == "hgt":
+        unit = value[-2:]
+        if unit != "in" and unit != "cm": return False
+        try:
+            val = int(value[:-2])
+        except:
+            return False
         
-invalids2 = 0
-for b in passports2:
-    vals = b.split(" ")
-    for g in vals:
-        v = g.split(":")
-        if v[0] == "byr":
-            if not v[1].isdigit:
-                invalids2 += 1
-                break
-            val = int(v[1])
-            if val < 1920 or val > 2002:
-                invalids2 += 1
-                break
-        if v[0] == "iyr":
-            if not v[1].isdigit:
-                invalids2 += 1
-                break
-            val = int(v[1])
-            if val < 2010 or val > 2020:
-                invalids2 += 1
-                break
-        if v[0] == "eyr":
-            if not v[1].isdigit:
-                invalids2 += 1
-                break
-            val = int(v[1])
-            if val < 2020 or val > 2030:
-                invalids2 += 1
-                break
-        if v[0] == "hgt":
-            if len(v[1]) < 4 or len(v[1]) > 5:
-                invalids2 += 1
-                break
-            if not v[1][:-2].isdigit:
-                invalids2 += 1
-                break
-            val = int(v[1][:-2])
-            unit = v[1][-2:]
-            if unit == "cm":
-                if val < 150 or val > 193:
-                    invalids2 += 1
-                    break   
-            elif unit == "in":
-                if val < 59 or val > 76:
-                    invalids2 += 1
-                    break
-            else:
-                invalids2 += 1
-                break
-        if v[0] == "hcl":
-            val = v[1]
-            if len(val) != 7 or val[0] != "#":
-                invalids2 += 1
-                break 
-            if not all(c in string.hexdigits for c in val[1:]):
-                invalids2 += 1
-                break  
-        if v[0] == "ecl": 
-            val = v[1]
-            if not val in eyecolors:
-                invalids2 += 1
-                break  
-        if v[0] == "pid":
-            val = v[1]
-            if len(val) != 9 or not val.isdigit():
-                invalids2 += 1
-                break  
+        if unit == "cm":
+            if not 150 <= val <= 193: return False 
+        if unit == "in":
+            if not 59 <= val <= 76: return False
+    if field == "hcl":
+        if len(value) != 7 or value[0] != "#": return False
+        if not all(c in string.hexdigits for c in value[1:]): return False
+    if field == "ecl":
+        if not value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]: return False
+    if field == "pid":
+        if len(value) != 9: return False
+    ## for now
+    if field == "cid": return False
 
-print("Valid :>", len(passports) - invalids)
-print("Valid2 :>", len(passports2) - invalids2)
+    return True
 
+for x in passports:
+    if checkfields(x): passports2.append(x)
+
+validtotal = 0
+for y in passports2:
+    data = y.split(" ")
+    validate = 0
+    for f in data:
+        v = f.split(":")
+        validate += checkvalues(v[0], v[1])
+    if validate == 7:
+        validtotal += 1
+
+print("Valid set of fields:>", len(passports2))
+print("Valid set of fields and values:>", validtotal)
 
 ## Print runtime
 et = time.time()
