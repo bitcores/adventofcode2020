c4a4e6e0a18d11fac8d904ffdc60ee7092817d15
bitcores <bitcores@gmail.com>
2020-12-04 11:49:24 +0900

    Add files via upload

--- d3bba.py
+++ d3bba.py
@@ -1,6 +1,7 @@
 import time
 from mpi4py import MPI
 import numpy as np
+from itertools import combinations
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
@@ -9,25 +10,21 @@ size = comm.size
 st = time.time()
 
 ## Functions
-def findtrees(dy):
-    ypos = 0
-    xpos = [0] * len(right)
-    trees = [0] * len(right)
-
-    while ypos < len(inp):
-        for dx in range(len(xpos)):
-            if inp[ypos][xpos[dx]] == "#":
-                trees[dx] += 1
-        
-        for x in range(len(xpos)):
-            xpos[x] = (xpos[x]+right[x]) % xlen
-        ypos += dy
-
-    return trees
+def findtrees(slope):
+    pos = [0,0]
+    ts = 0
+    while pos[0] < len(inp):
+        if inp[pos[0]][pos[1]] == "#":
+            ts += 1
+        pos[1] = (pos[1]+slope[1]) % xlen
+        pos[0] += slope[0] 
+    return ts
 
 if rank == 0:
     inp = []
     down = [1, 5, 7, 11, 13, 17, 19, 23, 25, 29, 31, 35, 37, 41, 47]
+    right = [2, 3, 4, 6, 8, 9, 12, 16, 18, 24, 32, 36, 48, 54, 64]
+    slopes = []
 
     ## Parse input
     with open("input3bb.txt") as fp:
@@ -36,30 +33,36 @@ if rank == 0:
             
             inp.append(line)
 
-    downc = np.array_split(down, size)
+    for y in down:
+        for x in right:
+            slopes.append([y,x])
+
+    slopet = np.array_split(slopes, size)
 else:
     inp = None
-    downc = None
+    slopet = None
 
 inp = comm.bcast(inp, root=0)
-downc = comm.scatter(downc, root=0)
+slopet = comm.scatter(slopet, root=0)
 
 ## Solve problem
 #[y,x]
 xlen = len(inp[0])
-right =[2, 3, 4, 6, 8, 9, 12, 16, 18, 24, 32, 36, 48, 54, 64]
 
-ml = []
-for d in downc: ml.append(findtrees(d))
+tl = []
+prod = 1
+for s in slopet:
+    trees = findtrees(s)
+    tl.append(trees)
+    prod *= trees
 
-sumlist = comm.gather(ml, root=0)
+sumlist = comm.gather(tl, root=0)
+prods = comm.gather(prod, root=0)
 
 if rank == 0:
     bbsum = 1
-    for o in sumlist: 
-        for p in o:
-            for q in p:
-                bbsum *= q
+    for o in prods: 
+        bbsum *= o
     print("Product of Bigboy Trees encountered:> ", bbsum)
     print(sumlist)
     #print(sum(sumlist))
