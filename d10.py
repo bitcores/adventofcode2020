af15d5e9b181dc38a86bcc22f4227a457e059956
bitcores <bitcores@gmail.com>
2020-12-11 06:33:26 +0900

    Add files via upload

--- d10.py
+++ d10.py
@@ -29,22 +29,8 @@ sj = 0
 inp.append(max(inp)+3)
 
 # part 1 was piss easy, did that myself no problem
-#1,3
-difs = [0,0]
-mdif = [1,3]
-prev = sj
-for x in inp:
-    if x - prev == mdif[0]:
-        difs[0] += 1
-    if x - prev == mdif[1]:
-        difs[1] += 1
-    prev = x
-
-print("Product of 1-jolt and 3-jolt diffs:> ", difs[0] * difs[1])
-
-
 ##
-# I needed a hint for thise one, it didn't make sense to me.
+# I needed a hint for part two, it didn't make sense to me.
 # Basically, you're going down the list of numbers counting the number of paths back to
 # the start point (0), and that number is the sum connections from the largest gap to
 # the smallest gap. Looking at the list 0, 1, 4, 5, 6, 7, 10 from the example.
@@ -63,11 +49,23 @@ print("Product of 1-jolt and 3-jolt diffs:> ", difs[0] * difs[1])
 # You could also use gaps 1-5 and x-1 to x-5, or gaps 4-7 with x-4 to x-7 but I don't
 # think it works properly if there were negative numbers. Anyway, I learned something.
 ##
+#1,3
+difs = [0,0]
+mdif = [1,3]
+prev = sj
 dp = defaultdict(int)
 dp[sj] = 1
 for x in inp:
+    if x - prev == mdif[0]:
+        difs[0] += 1
+    if x - prev == mdif[1]:
+        difs[1] += 1
+    prev = x
+
     for d in range(mdif[0], mdif[1]+1):
         dp[x] += dp[x-d]
+
+print("Product of 1-jolt and 3-jolt diffs:> ", difs[0] * difs[1])
 print("No. of distict arrangements of adapters:> ", dp[max(inp)])
 
 
