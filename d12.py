1d30adf0e5e7556916c02773f86ee7c47728ad8e
bitcores <bitcores@gmail.com>
2020-12-12 22:51:03 +0900

    Add files via upload

--- d12.py
+++ d12.py
@@ -34,107 +34,47 @@ def changedir(a, s):
         d -= r
         if d < 0:
             d += 4
+    
+    for _ in range(0, r):
+        if a == "L":
+            wp[0], wp[1] = wp[1], wp[0]*-1
+        if a == "R":
+            wp[0], wp[1] = wp[1]*-1, wp[0]
 
-def movedir(a, s):
-    global d
-
-    if a == "F":
-        cpos[0] += dl[d][0] * s
-        cpos[1] += dl[d][1] * s
-    elif a == "E":
-        cpos[0] += dl[0][0] * s
-        cpos[1] += dl[0][1] * s
-    elif a == "N":
-        cpos[0] += dl[1][0] * s
-        cpos[1] += dl[1][1] * s
-    elif a == "W":
-        cpos[0] += dl[2][0] * s
-        cpos[1] += dl[2][1] * s
-    elif a == "S":
-        cpos[0] += dl[3][0] * s
-        cpos[1] += dl[3][1] * s
-
-def movewp(a, s):
+def movedir(a, s, p):
     global d
 
     if a == "E":
-        wp[0] += dl[0][0] * s
-        wp[1] += dl[0][1] * s
+        p[1] += dl[0][1] * s
     elif a == "N":
-        wp[0] += dl[1][0] * s
-        wp[1] += dl[1][1] * s
+        p[0] += dl[1][0] * s
     elif a == "W":
-        wp[0] += dl[2][0] * s
-        wp[1] += dl[2][1] * s
+        p[1] += dl[2][1] * s
     elif a == "S":
-        wp[0] += dl[3][0] * s
-        wp[1] += dl[3][1] * s
-
-def rotatewp(a, s):
-    global d
-    r = s // 90
-    nd = d
-    if a == "L":
-        nd += r
-        if nd > 3:
-            nd -= 4
-    if a == "R":
-        nd -= r
-        if nd < 0:
-            nd += 4
-
-    if r == 2:
-        wp[0] = wp[0] * -1
-        wp[1] = wp[1] * -1
-    elif (a == "L" and r == 1) or (a == "R" and r == 3):
-        t = wp[0]
-        wp[0] = wp[1]
-        wp[1] = t
-        if d == 0 or d == 2:
-            wp[1] *= -1
-        if d == 1 or d == 3:
-            wp[1] *= -1
-    elif (a == "R" and r == 1) or (a == "L" and r == 3):
-        t = wp[1]
-        wp[1] = wp[0]
-        wp[0] = t
-        if d == 0 or d == 2:
-            wp[0] *= -1
-        if d == 1 or d == 3:
-            wp[0] *= -1
-
-    d = nd
+        p[0] += dl[3][0] * s
 
 dl = [[0,1], [1,0], [0,-1], [-1,0]]
 d = 0
-cpos = [0,0]
+cpos1 = [0,0]
+cpos2 = [0,0]
+wp = [1,10]
 for m in inp:
     a = m[0]
     s = int(m[1:])
 
     if a == "L" or a == "R":
         changedir(a, s)
-    else:
-        movedir(a, s)
-    
-print(abs(cpos[0]) + abs(cpos[1]))
-
-d = 0
-wp = [1,10]
-cpos = [0,0]
-for m in inp:
-    a = m[0]
-    s = int(m[1:])
-
-    if a == "L" or a == "R":
-        rotatewp(a, s)
     elif a == "F":
-        cpos[0] += wp[0] * s
-        cpos[1] += wp[1] * s
+        cpos1[0] += dl[d][0] * s
+        cpos1[1] += dl[d][1] * s
+        cpos2[0] += wp[0] * s
+        cpos2[1] += wp[1] * s
     else:
-        movewp(a, s)
-
-print(abs(cpos[0]) + abs(cpos[1]))
+        movedir(a, s, cpos1)
+        movedir(a, s, wp)
+    
+print("Manhattan distance:> ", abs(cpos1[0]) + abs(cpos1[1]))
+print("Following waypoint:> ", abs(cpos2[0]) + abs(cpos2[1]))
 
 
 ## Print runtime
