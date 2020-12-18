0567ee0902e47cc79cedf701d82723587d08b4a0
bitcores <bitcores@gmail.com>
2020-12-18 10:39:09 +0900

    Add files via upload

--- d17.py
+++ d17.py
@@ -3,10 +3,11 @@ from collections import defaultdict
 from copy import deepcopy
 st = time.time()
 
-inputfile = "input17t.txt"
+inputfile = "input17.txt"
 
 def readinput():
-    L = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: ".")))
+    L = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: "."))))
+    w = 0
     z = 0
     y = 0
     with open(inputfile) as fp:
@@ -14,7 +15,7 @@ def readinput():
             line = line.strip()
 
             for x,v in enumerate(line):
-                L[z][y][x] = v
+                L[w][z][y][x] = v
             y += 1
             
     return L
@@ -32,13 +33,14 @@ inp = readinput()
 conway = deepcopy(inp)
 
 def printslices():
-    for z in conway:
-        print(z)
-        for y in range(ymm[0], ymm[1]+1):
-            line = ""
-            for x in range(xmm[0], xmm[1]+1):
-                line += conway[z][y][x]
-            print(line)
+    for w in conway:
+        for z in conway[w]:
+            print(z)
+            for y in range(ymm[0], ymm[1]+1):
+                line = ""
+                for x in range(xmm[0], xmm[1]+1):
+                    line += conway[w][z][y][x]
+                print(line)
 
 def spotval(pos):
     if not pos[0] in conway:
@@ -47,51 +49,63 @@ def spotval(pos):
         return "."
     if not pos[2] in conway[pos[0]][pos[1]]:
         return "."
-    return conway[pos[0]][pos[1]][pos[2]]
+    if not pos[3] in conway[pos[0]][pos[1]][pos[2]]:
+        return "."
+    return conway[pos[0]][pos[1]][pos[2]][pos[3]]
 
 def checkneighbors(pos):
     nbr = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1], [0,0]]
-    posz, posy, posx = pos[0], pos[1], pos[2]
+    posw, posz, posy, posx = pos[0], pos[1], pos[2], pos[3]
     actvcnt = 0
-    for cz in range(-1, 2):
-        for nn in nbr:
-            if cz == 0 and nn == [0,0]:
-                continue
-            if not posz+cz in conway:
-                continue
-            if not posy+nn[0] in conway[posz+cz]:
-                continue
-            if not posx+nn[1] in conway[posz+cz][posy+nn[0]]:
-                continue
-            if conway[posz+cz][posy+nn[0]][posx+nn[1]] == "#":
-                actvcnt += 1
+    for cw in range(-1, 2):
+        for cz in range(-1, 2):
+            for nn in nbr:
+                if cz == 0 and nn == [0,0] and cw == 0:
+                    continue
+                if not posw+cw in conway:
+                    continue
+                if not posz+cz in conway[posw+cw]:
+                    continue
+                if not posy+nn[0] in conway[posw+cw][posz+cz]:
+                    continue
+                if not posx+nn[1] in conway[posw+cw][posz+cz][posy+nn[0]]:
+                    continue
+                if conway[posw+cw][posz+cz][posy+nn[0]][posx+nn[1]] == "#":
+                    actvcnt += 1
     return actvcnt
 
 ## store min/max bounds for z.y.x before start of loop and then update from updatelist
 
 
-zmm = [min(conway), max(conway)]
-ymm = [min(conway[0]), max(conway[0])]
-xmm = [min(conway[0][0]), max(conway[0][0])]
+wmm = [min(conway), max(conway)]
+zmm = [min(conway[0]), max(conway[0])]
+ymm = [min(conway[0][0]), max(conway[0][0])]
+xmm = [min(conway[0][0][0]), max(conway[0][0][0])]
 
 for _ in range(0,6):
     
     updatelist = []
-    
-    for ddz in range(zmm[0]-1, xmm[1]+2):
-        for ddy in range(ymm[0]-1, ymm[1]+2):
-            for ddx in range(xmm[0]-1, xmm[1]+2):
-                res = checkneighbors([ddz,ddy,ddx])
-                if spotval([ddz,ddy,ddx]) == ".":
-                    if res == 3:
-                        updatelist.append(([ddz,ddy,ddx], "#"))
-                if spotval([ddz,ddy,ddx]) == "#":
-                    if not res == 2 and not res == 3:
-                        updatelist.append(([ddz,ddy,ddx], "."))  
+    # set ddw range to 0,1 for part 1, wmm[0]-1, wmm[1]+2 for part 2
+    for ddw in range(wmm[0]-1, wmm[1]+2):
+        for ddz in range(zmm[0]-1, zmm[1]+2):
+            for ddy in range(ymm[0]-1, ymm[1]+2):
+                for ddx in range(xmm[0]-1, xmm[1]+2):
+                    res = checkneighbors([ddw,ddz,ddy,ddx])
+                    if spotval([ddw,ddz,ddy,ddx]) == ".":
+                        if res == 3:
+                            updatelist.append(([ddw,ddz,ddy,ddx], "#"))
+                    if spotval([ddw,ddz,ddy,ddx]) == "#":
+                        if not res == 2 and not res == 3:
+                            updatelist.append(([ddw,ddz,ddy,ddx], "."))  
 
     for rl in updatelist:
         pos = rl[0]
-        pz,py,px = pos[0],pos[1],pos[2]
+        pw,pz,py,px = pos[0],pos[1],pos[2],pos[3]
+        if not wmm[0] <= pw <= wmm[1]:
+            if pw < wmm[0]:
+                wmm[0] = pw
+            if pw > wmm[1]:
+                wmm[1] = pw
         if not zmm[0] <= pz <= zmm[1]:
             if pz < zmm[0]:
                 zmm[0] = pz
@@ -107,16 +121,19 @@ for _ in range(0,6):
                 xmm[0] = px
             if px > xmm[1]:
                 xmm[1] = px  
-        conway[pz][py][px] = rl[1]
+        conway[pw][pz][py][px] = rl[1]
+
+    #printslices()
+    #input()
 
 sumcube = 0
-for z in conway:
-    for y in conway[z]:
-        sumcube += sum(value == "#" for value in conway[z][y].values())
+for w in conway:
+    for z in conway[w]:
+        for y in conway[w][z]:
+            sumcube += sum(value == "#" for value in conway[w][z][y].values())
 #print(updatelist)
 print(sumcube)
-#printslices()
-#input()
+
 
 ## Print runtime
 et = time.time()
